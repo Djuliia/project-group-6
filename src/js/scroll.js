707d477e2af164155aa029e2d5f37cf3f2fbd951
Yuliia Chaika <124448012+Djuliia@users.noreply.github.com>
2023-04-25 23:16:56 +0300

    const offset = 20 to 40

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -3,7 +3,7 @@
  */
 function scrollToSection() {
     function smoothScroll(targetEl, duration) {
-        const offset = 20;
+        const offset = 40;
         let target = document.querySelector(targetEl);
         let targetPosition = target.getBoundingClientRect().top - offset;
         let startPosition = window.pageYOffset;
