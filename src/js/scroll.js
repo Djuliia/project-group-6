64c7eb436db59f9a5817d3c63cfbd4dc23639a58
Yuliia Chaika <124448012+Djuliia@users.noreply.github.com>
2023-04-25 22:04:46 +0300

    Create scroll.js

---
+++ src/js/scroll.js
@@ -0,0 +1,42 @@
+/**
+ * Scroll to section
+ */
+function scrollToSection() {
+    function smoothScroll(targetEl, duration) {
+        const offset = 20;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - offset;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+        const ease = function (t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+        const animation = function (currentTime) {
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0, run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+    };
+    const links = document.querySelectorAll('a.scroll-to'),
+        mbMenu = document.querySelector('.js-menu-container');
+    if (links) {
+        links.forEach(link => {
+            link.addEventListener('click', function (e) {
+                e.preventDefault();
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+                if (mbMenu && mbMenu.classList.contains('is-open')) {
+                    mbMenu.classList.remove('is-open')
+                    document.body.removeAttribute('style')
+                }
+            });
+        });
+    }
+}
+scrollToSection();
