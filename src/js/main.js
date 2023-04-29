48e18bf79fc9f66b4db5949d6e54db049cb625fd
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-04-29 17:35:28 +0300

    add selection.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,4 @@ import './modal.js';
 import './swiper.js';
 import './scroll.js';
 import './novelty.js';
-
+import './selection.js';
---
+++ src/js/selection.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector('.modal-item');
+
+window.addEventListener('click', function (event) {
+  if (event.target.hasAttribute('data-cart')) {
+    const card = event.target.closest('.catalog-card');
+    const productInfo = {
+      id: card.dataset.id,
+      imgSrc: card.querySelector('.watch-image').getAttribute('src'),
+      title: card.querySelector('.title-card').innerText,
+      price: card.querySelector('.price').innerText,
+    };
+    const cartItemHTML = `<div data-id="${productInfo.id}">
+			<img class="watch-image" src="${productInfo.imgSrc}" alt="Steel watch" width="276"/>
+			<h3 class="title-card">${productInfo.title}</h3>
+			<p class="price">${productInfo.price}</p>
+			</div>`;
+    modal.insertAdjacentHTML('beforeend', cartItemHTML);
+  }
+});
